26955754c135de456299e6df498c0bc30a98832e
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-07-06 23:26:55 +0100

    Wrap injection code in anon function scope

--- injection.js
+++ injection.js
@@ -1,96 +1,100 @@
-function styled_log(...texts) {
-  console.log(
-    '%cAniList Modifier%c ' + texts.join(' '),
-    'background-color: #DD4124; border-radius: 5px; padding: 0.2em 0.3em; color: white; font-family: Verdana;',
-    ''
-  );
-}
-
-async function background_api_call(feed_items) {
-  try {
-    return await chrome.runtime.sendMessage('', {
-      action: 'fetch-scores',
-      feed_items
-    });
-  } catch (e) {
-    styled_log("Couldn't connect to server");
-    console.error(e);
-    return [];
-  }
-}
-
-const update_feed_items = feed_items => {
-  feed_items = feed_items
-    .filter(f => {
-      const status_text = f.querySelector('.status').innerText;
-      return (
-        status_text.startsWith('Completed') ||
-        status_text.startsWith('Dropped') ||
-        status_text.startsWith('Rewatched')
-      );
-    })
-    .map(f => ({
-      user: f.querySelector('.name').innerText,
-      id: parseInt(
-        f
-          .querySelector('a.cover')
-          .href.split(/anilist\.co\/(anime|manga)\//)[2]
-          .split('/')[0]
-      ),
-      status_el: f.querySelector('.status')
-    }));
-
-  if (!feed_items.length) return;
-
-  background_api_call(feed_items).then(
-    ({ scores, api_calls_left, api_calls_total }) => {
-      styled_log(
-        `Updating ${feed_items.length} feed items... (${api_calls_left}/${api_calls_total} API calls remaining)`
-      );
-
-      scores.forEach(({ user, id, score }) => {
-        const status_el = feed_items.find(
-          fi => fi.user === user && fi.id === id
-        ).status_el;
-
-        if (score) {
-          status_el.innerHTML += ` and rated it a${
-            score >= 8 && score < 9 ? 'n' : ''
-          } <b>${score}</b>.`;
-        } else {
-          status_el.innerHTML += ` without rating it.`;
-        }
-      });
-    }
-  );
-};
-
 (() => {
-  styled_log('Loading Extension...');
-  update_feed_items([...document.querySelector('.activity-feed').children]);
+  function styled_log(...texts) {
+    console.log(
+      '%cAniList Modifier%c ' + texts.join(' '),
+      'background-color: #DD4124; border-radius: 5px; padding: 0.2em 0.3em; color: white; font-family: Verdana;',
+      ''
+    );
+  }
 
-  const observer = new MutationObserver(mutationsList => {
-    const new_feed_items = [];
-
-    for (const mutation of mutationsList) {
-      if (mutation.type == 'childList') {
-        new_feed_items.push(
-          ...[...mutation.addedNodes].filter(
-            new_node =>
-              new_node.classList &&
-              (new_node.classList.contains('activity-anime_list') ||
-                new_node.classList.contains('activity-manga_list'))
-          )
-        );
-      }
+  async function background_api_call(feed_items) {
+    try {
+      return await chrome.runtime.sendMessage('', {
+        action: 'fetch-scores',
+        feed_items
+      });
+    } catch (e) {
+      styled_log("Couldn't connect to server");
+      console.error(e);
+      return [];
     }
+  }
 
-    if (new_feed_items.length) update_feed_items(new_feed_items);
-  });
+  function update_feed_items(feed_items) {
+    feed_items = feed_items
+      .filter(f => {
+        const status_text = f.querySelector('.status').innerText;
+        return (
+          status_text.startsWith('Completed') ||
+          status_text.startsWith('Dropped') ||
+          status_text.startsWith('Rewatched')
+        );
+      })
+      .map(f => ({
+        user: f.querySelector('.name').innerText,
+        id: parseInt(
+          f
+            .querySelector('a.cover')
+            .href.split(/anilist\.co\/(anime|manga)\//)[2]
+            .split('/')[0]
+        ),
+        status_el: f.querySelector('.status')
+      }));
 
-  observer.observe(document.querySelector('.activity-feed'), {
-    attributes: true,
-    childList: true,
-    subtree: true
-  });
+    if (!feed_items.length) return;
+
+    background_api_call(feed_items).then(
+      ({ scores, api_calls_left, api_calls_total }) => {
+        styled_log(
+          `Updating ${feed_items.length} feed items... (${api_calls_left}/${api_calls_total} API calls remaining)`
+        );
+
+        scores.forEach(({ user, id, score }) => {
+          const status_el = feed_items.find(
+            fi => fi.user === user && fi.id === id
+          ).status_el;
+
+          if (score) {
+            status_el.innerHTML += ` and rated it a${
+              score >= 8 && score < 9 ? 'n' : ''
+            } <b>${score}</b>.`;
+          } else {
+            status_el.innerHTML += ` without rating it.`;
+          }
+        });
+      }
+    );
+  }
+
+  function main() {
+    styled_log('Loading Extension...');
+    update_feed_items([...document.querySelector('.activity-feed').children]);
+
+    const observer = new MutationObserver(mutationsList => {
+      const new_feed_items = [];
+
+      for (const mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+          new_feed_items.push(
+            ...[...mutation.addedNodes].filter(
+              new_node =>
+                new_node.classList &&
+                (new_node.classList.contains('activity-anime_list') ||
+                  new_node.classList.contains('activity-manga_list'))
+            )
+          );
+        }
+      }
+
+      if (new_feed_items.length) update_feed_items(new_feed_items);
+    });
+
+    observer.observe(document.querySelector('.activity-feed'), {
+      attributes: true,
+      childList: true,
+      subtree: true
+    });
+  }
+
+  main();
 })();
