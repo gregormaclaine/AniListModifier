9b64c04d8e589256581879e6f68d68543c84b49b
Gregor Maclaine <thegregormaclaine@gmail.com>
2024-05-20 23:44:47 +0200

    Removed unused setting

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,6 @@ export type ExtensionSettings = {
   verbose: boolean;
   rating_colors: 'off' | 'interpolated' | 'custom';
   custom_colors: string[];
-  interp_saturation: number;
   autoscroll: boolean;
   scored_feed: boolean;
 };
@@ -15,7 +14,6 @@ export function default_settings(): ExtensionSettings {
     autoscroll: true,
     scored_feed: true,
     rating_colors: 'interpolated',
-    interp_saturation: 100,
     custom_colors: Array(10)
       .fill(0)
       .map((_, i) => get_color(i / 9, 65))
