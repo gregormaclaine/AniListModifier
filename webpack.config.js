bdb7fc3c02278a05019ab61fb333cfd58da4d7f5
Gregor Maclaine <thegregormaclaine@gmail.com>
2024-05-22 23:31:50 +0200

    Moved babel plugins from proposals to standards
    (cherry picked from commit 4023220455ed00b5f11ed3831de91eaad3fa641c)

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,8 @@ module.exports = {
             presets: ['@babel/preset-env', 'solid', '@babel/preset-typescript'],
             plugins: [
               '@babel/plugin-syntax-dynamic-import',
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-object-rest-spread'
+              '@babel/plugin-transform-class-properties',
+              '@babel/plugin-transform-object-rest-spread'
             ]
           }
         }
