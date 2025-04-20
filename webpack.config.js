0f84c0d72544683b2a0cd477603d8a6da095952b
Gregor Maclaine <thegregormaclaine@gmail.com>
2025-04-20 01:52:47 +0100

    Edit build config to modify manifest if building for firefox

--- webpack.config.js
+++ webpack.config.js
@@ -5,87 +5,107 @@ const TransformJson = require('transform-json-webpack-plugin');
 
 const VERSION = require('./package.json').version;
 
-module.exports = {
-  mode: 'production',
-  // devtool: 'cheap-module-source-map',
-  entry: {
-    background: path.resolve(__dirname, 'src', 'background', 'index.ts'),
-    injection: path.resolve(__dirname, 'src', 'content', 'index.ts'),
-    'templates/settings/main': path.resolve(
-      __dirname,
-      'src',
-      'content',
-      'templates',
-      'settings',
-      'index.ts'
-    )
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            configFile: false,
-            presets: ['@babel/preset-env', 'solid', '@babel/preset-typescript'],
-            plugins: [
-              '@babel/plugin-syntax-dynamic-import',
-              '@babel/plugin-transform-class-properties',
-              '@babel/plugin-transform-object-rest-spread'
-            ]
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: '.', to: '.', context: 'public' },
+module.exports = env => {
+  const isFirefox = env.browser === 'firefox';
+  console.log(
+    isFirefox
+      ? '>>> 🦊 Building for Firefox <<<\n'
+      : '>>> 🟠 Building for Chrome <<<\n'
+  );
+
+  return {
+    mode: 'production',
+    // devtool: 'cheap-module-source-map',
+    entry: {
+      background: path.resolve(__dirname, 'src', 'background', 'index.ts'),
+      injection: path.resolve(__dirname, 'src', 'content', 'index.ts'),
+      'templates/settings/main': path.resolve(
+        __dirname,
+        'src',
+        'content',
+        'templates',
+        'settings',
+        'index.ts'
+      )
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx']
+    },
+    module: {
+      rules: [
         {
-          from: './src/popup',
-          to: './popup',
-          filter: f => !f.match(/\.tsx?$/),
-          transform(content, file) {
-            if (file.endsWith('.html')) {
-              return content.toString().replace(/{{version}}/g, 'v' + VERSION);
-            } else {
-              return content;
-            }
-          }
-        },
-        {
-          from: './src/content/templates',
-          to: './templates',
-          filter: f => !f.match(/\.tsx?$/),
-          transform(content, file) {
-            if (file.endsWith('.html')) {
-              return content.toString().replace(/{{version}}/g, 'v' + VERSION);
-            } else {
-              return content;
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              configFile: false,
+              presets: [
+                '@babel/preset-env',
+                'solid',
+                '@babel/preset-typescript'
+              ],
+              plugins: [
+                '@babel/plugin-syntax-dynamic-import',
+                '@babel/plugin-transform-class-properties',
+                '@babel/plugin-transform-object-rest-spread'
+              ]
             }
           }
         }
       ]
-    }),
-    new TransformJson({
-      filename: 'manifest.json',
-      source: 'public/manifest.json',
-      object: { version: VERSION }
-    })
-  ]
-  // optimization: {
-  //   minimize: false
-  // }
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyPlugin({
+        patterns: [
+          { from: '.', to: '.', context: 'public' },
+          {
+            from: './src/popup',
+            to: './popup',
+            filter: f => !f.match(/\.tsx?$/),
+            transform(content, file) {
+              if (file.endsWith('.html')) {
+                return content
+                  .toString()
+                  .replace(/{{version}}/g, 'v' + VERSION);
+              } else {
+                return content;
+              }
+            }
+          },
+          {
+            from: './src/content/templates',
+            to: './templates',
+            filter: f => !f.match(/\.tsx?$/),
+            transform(content, file) {
+              if (file.endsWith('.html')) {
+                return content
+                  .toString()
+                  .replace(/{{version}}/g, 'v' + VERSION);
+              } else {
+                return content;
+              }
+            }
+          }
+        ]
+      }),
+      new TransformJson({
+        filename: 'manifest.json',
+        source: 'public/manifest.json',
+        object: {
+          version: VERSION,
+          ...(isFirefox ? { background: { scripts: ['background.js'] } } : {})
+        }
+      })
+    ]
+    // optimization: {
+    //   minimize: false
+    // }
+  };
 };
